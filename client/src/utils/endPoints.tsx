0890533533a9ce984c3cd2addbb13c8d20dc3078
Kittipong2001 <128574773+Kittipong2001@users.noreply.github.com>
2024-08-04 03:05:05 +0700

    add methods to client

--- client/src/utils/endPoints.tsx
+++ client/src/utils/endPoints.tsx
@@ -3,6 +3,7 @@ const endPoints = [
 	{ name: "False Position", api: "falsi" },
 	{ name: "One-iteration", api: "fixed" },
 	{ name: "Newton Raphson", api: "newton" },
+	{ name: "Secant", api: "secant" },
 ];
 
 export default endPoints;
